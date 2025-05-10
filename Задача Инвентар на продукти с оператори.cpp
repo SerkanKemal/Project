cfe0812200eaf71adc7a3b11cae7a368acf00839
Serkan Kemal <14024123@naval-acad.bg>
2025-05-10 15:36:17 +0300

    Add files via upload

---
+++ Задача Инвентар на продукти с оператори.cpp
@@ -0,0 +1,94 @@
+﻿// Задача Инвентар на продукти с оператори.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Product {
+public:
+    string name;
+    double price;
+    int quantity;
+
+    Product(string n,double p, int q) {
+        name = n;
+        price = p;
+        quantity = q;
+    }
+
+    bool operator==(const Product& other) const {
+        return name == other.name && price == other.price;
+    }
+
+    double GetTotalValue() const {
+        return price * quantity;
+    }
+
+    void printinfo() {
+        cout << "Name: " << name << " Price: " << price << endl;
+    }
+
+};
+
+ostream& operator<<(ostream& os, const Product& p) {
+    os << "Product: " << p.name << ", Price: " << p.price;
+    return os;
+}
+
+int main()
+{
+    vector<Product> products = {
+        Product("Keyboard",10.99,10),
+        Product("Keyboard",10.99,10),
+        Product("Mouse",9.99,12),
+        Product("PC",119.99,6),
+        Product("MousePad",4.99,20),
+    };
+
+    cout << "All Products: " << endl;
+
+    for (int i = 0; i < products.size(); i++) {
+        cout << products.at(i) << endl;
+    }
+
+    bool foundByNameAndPrice = false;
+
+    for (int i = 0; i < products.size(); i++) {
+        for (int j = i + 1; j < products.size(); j++) {
+            if (products.at(i) == products.at(j)) {
+                cout << "These products are same: " << endl;
+                cout << products.at(i) << endl;
+                cout << products.at(j) << endl;
+                foundByNameAndPrice = true;
+            }
+        }
+    }
+
+    if (!foundByNameAndPrice) {
+        cout << "No same producst" << endl;
+    }
+
+    double totalSum = 0;
+
+    for (int i = 0; i < products.size(); i++) {
+        totalSum = totalSum + products.at(i).GetTotalValue();
+    }
+
+    cout << totalSum;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Задача Управление на инвентар в склад.cpp
@@ -0,0 +1,104 @@
+﻿// Задача Управление на инвентар в склад.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class item {
+public:
+    string name;
+    double price;
+    int quantity;
+
+    item(string n, double p, int q) {
+        name = n;
+        price = p;
+        quantity = q;
+    }
+
+    void printinfo() {
+        cout << "|Item:" << name << "|Price:" << price << "|Quantity:" << quantity << endl;
+    }
+
+    bool isLowStock(int kolicestvo) {
+        return quantity < kolicestvo;
+    }
+
+    double TotalValue() const {
+        return price * quantity;
+    }
+
+    bool foundByName(const string& name2) const {
+        return name == name2;
+    }
+};
+
+int main()
+{
+    vector<item> items = {
+     item("Mouse",20,10),
+     item("Monitor",100,20),
+     item("PC",500,100),
+     item("Keyboard",80,15),
+     item("Mic",90,20)
+    };
+
+    int kolicestvo;
+    cout << "How much threshold: ";
+    cin >> kolicestvo;
+
+    bool foundkolicestvo = false;
+
+    cout << "Result for threshold: " << endl;
+
+    for (int i = 0; i < items.size(); i++) {
+        if (items.at(i).isLowStock(kolicestvo)) {
+            items.at(i).printinfo();
+            foundkolicestvo = true;
+        }
+    }
+    if (!foundkolicestvo) {
+        cout << "No items below threshold!" << endl;
+    }
+
+    double total = 0;
+
+    for (int i = 0; i < items.size(); i++) {
+        total = total + items.at(i).TotalValue();
+    }
+    cout << "Total is: " << total << endl;
+
+    string name2;
+    cout << "Enter item ur looking for: ";
+    cin >> name2;
+
+    bool foundByName2 = false;
+
+    cout << "Result from searching by name: " << endl;
+    for (int i = 0; i < items.size(); i++) {
+        if (items.at(i).foundByName(name2)) {
+            items.at(i).printinfo();
+            foundByName2 = true;
+        }
+    }
+    if (!foundByName2) {
+        cout << "There is no item by this name: " << endl;
+    }
+   
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
